8e5b62e9e1d491f4a07388c9667181af2874715f
Yunfan Liu <yunfan@umich.edu>
2017-02-22 17:18:07 -0500

    Started to work on Torch

---
+++ Code/main.lua
@@ -0,0 +1,7 @@
+-- The organization of the project is largely borrowed from 'https://github.com/anewell/pose-hg-train'
+-- Thanks for the great work of Alejandro Newell on Human Pose Estimation!
+
+
+-- Initialization
+require 'paths'
+paths.dofile('ref.lua') -- Do global variable initialization and parse the command line input
---
+++ Code/ref.lua
@@ -0,0 +1,16 @@
+-- Import libs that could be useful
+require 'torch'
+require 'xlua'
+require 'optim'
+require 'nn'
+require 'nnx'
+require 'nngraph'
+require 'hdf5'
+require 'string'
+require 'image'
+ffi = require 'ffi'
+torch.setdefaulttensortype('torch.FloatTensor')
+
+-- Set the working dir
+projectDir = paths.concat(paths.cwd(), '/..')
+
