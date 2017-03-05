e375eb6a564ed39ca100014891d1d096a9fd2f9a
rayhou <rayhou@umich.edu>
2017-03-05 16:07:29 -0500

    cuda enabled

--- Code/testFCN8.lua
+++ Code/testFCN8.lua
@@ -11,11 +11,13 @@ print(#testData)
 predicted = fcn_net:forward(testData)
 print(#predicted)
 
-
+-- Liang's directory
 -- test_img_file = './VOC2011/JPEGImages/2007_000032.jpg'
-test_img_file = './VOC2011/JPEGImages/2007_000033.jpg'
+-- test_img_file = './VOC2011/JPEGImages/2007_000033.jpg'
+-- Ray's directory
+test_img_file = '../../Dataset/TrainVal/VOC2011/JPEGImages/2007_000033.jpg'
 testImage = image.load(test_img_file, 3, 'byte')
-testImage = testImage:double()
+testImage = testImage:cuda()
 print('----------------------------------------------------')
 print(#testImage)
 predictedLabel = fcn_net:forward(testImage)
